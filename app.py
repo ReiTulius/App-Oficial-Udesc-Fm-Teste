38ce6bc8a0e9d98230be3314d3ab5eb33a2b94f6
ReiTulius <heytuliusmusic@gmail.com>
2026-05-26 11:50:29 -0300

    app.py

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ from io import StringIO
 st.set_page_config(page_title="Painel Udesc FM - Mídias", page_icon="📻", layout="wide")
 
 # ==========================================
+# 📊 LINKS DAS PLANILHAS GOOGLE (DECLARAÇÃO INICIAL)
+# ==========================================
+URL_CARGA_SOM_DA_ILHA = "https://docs.google.com/spreadsheets/d/1zw7RPhpuInL7JqSylB_zOMu5zaqO4KgnJ7sD2eoM6gs/edit?usp=drive_link"
+URL_CARGA_TULIO = "https://docs.google.com/spreadsheets/d/16inPMqGCr50-MNJvwV1R4bykDgEGRwlxdbjWrlW6mfY/edit?usp=drive_link"
+URL_CARGA_JESSICA = "https://docs.google.com/spreadsheets/d/1MQ7OcghWNTZwaYVBTmZlMojYTXZMOe5vT1px5VALpS0/edit?usp=drive_link"
+
+URL_INSTAGRAM_SHEETS = URL_CARGA_SOM_DA_ILHA
+
+# ==========================================
 # ⚙️ CONFIGURAÇÕES DE CONEXÃO (GITHUB SECRETS)
 # ==========================================
 try:
@@ -24,8 +33,10 @@ ARQUIVO_BANCO = "acervo_udesc.csv"
 URL_API_GITHUB = f"https://api.github.com/repos/{REPOSITORIO}/contents/{ARQUIVO_BANCO}"
 
 # ==========================================
-# 📊 LINKS DAS PLANILHAS GOOGLE
+# 💾 REGRAS DE LEITURA E ESCRITA
 # ==========================================
-URL_CARGA_SOM_DA_ILHA = "https://docs.google.com/spreadsheets/d/1zw7RPhpuInL7JqSylB_zOMu5zaqO4KgnJ7sD2eoM6gs/edit?usp=drive_link"
-URL_CARGA_TULIO = "https://docs.google.com/spreadsheets/d/16inPMqGCr50-MNJvwV1R4bykDgEGRwlxdbjWrlW6mfY/edit?usp=drive_link"
-URL_CARGA_JESSICA
+def converter_link_google(url):
+    if "docs.google.com/spreadsheets" in url:
+        try:
+            id_planilha = url.split("/d/")[1].split("/")[0]
+            return f"
