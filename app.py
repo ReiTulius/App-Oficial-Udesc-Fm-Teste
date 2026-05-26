6f29d73f939a5353f71c07da4aa42d08ab92ec42
ReiTulius <heytuliusmusic@gmail.com>
2026-05-26 11:51:46 -0300

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from io import StringIO
 st.set_page_config(page_title="Painel Udesc FM - Mídias", page_icon="📻", layout="wide")
 
 # ==========================================
-# 📊 LINKS DAS PLANILHAS GOOGLE (DECLARAÇÃO INICIAL)
+# 📊 LINKS DAS PLANILHAS GOOGLE
 # ==========================================
 URL_CARGA_SOM_DA_ILHA = "https://docs.google.com/spreadsheets/d/1zw7RPhpuInL7JqSylB_zOMu5zaqO4KgnJ7sD2eoM6gs/edit?usp=drive_link"
 URL_CARGA_TULIO = "https://docs.google.com/spreadsheets/d/16inPMqGCr50-MNJvwV1R4bykDgEGRwlxdbjWrlW6mfY/edit?usp=drive_link"
@@ -30,13 +30,4 @@ except Exception:
     st.stop()
 
 ARQUIVO_BANCO = "acervo_udesc.csv"
-URL_API_GITHUB = f"https://api.github.com/repos/{REPOSITORIO}/contents/{ARQUIVO_BANCO}"
-
-# ==========================================
-# 💾 REGRAS DE LEITURA E ESCRITA
-# ==========================================
-def converter_link_google(url):
-    if "docs.google.com/spreadsheets" in url:
-        try:
-            id_planilha = url.split("/d/")[1].split("/")[0]
-            return f"
+URL_API_GITHUB = f"https://api.
